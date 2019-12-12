a6ab8f7a5f1ceeddd6665ffc48a47f966ad3b6f2
hubert-mazur <hiusvii@gmail.com>
2019-12-12 13:48:46 +0100

    bug fix

--- Lab_09/Draw.java
+++ Lab_09/Draw.java
@@ -40,7 +40,9 @@ public class Draw {
 		}
 
 
-		lengths = new ArrayList<Double>(numberOfPoints);
+		lengths = new ArrayList<Double>();
+		for  (int i =0;i<numberOfPoints;i++)
+		lengths.add(0.0);
 	}
 
 	public void run() {
@@ -49,16 +51,18 @@ public class Draw {
 			str = new PrintStream(fileOut);
 		} catch(FileNotFoundException e) {return;}
 
-		str.print("P3");
-		str.print("#Hubert Mazur");
-		str.print(xSize + " " + ySize);
-		str.print("255");
+		str.print("P3\n");
+		str.print("#Hubert Mazur\n");
+		str.print(xSize + " " + ySize + "\n");
+		str.print("255\n");
 
 
 		for (int i=0;i<xSize;i++) {
 			for (int j=0;j<ySize;j++) {
 				for (int p=0;p<numberOfPoints;p++) {
 				lengths.set(p, Math.pow(i - points.get(p).get(0),2) + Math.pow(j - points.get(p).get(1), 2));
+				// lengths.set(p,0.0);
+
 				if (i == points.get(p).get(0) && j == points.get(p).get(1)) {
 					str.print("255 0 0 ");
 					continue;
@@ -69,14 +73,14 @@ public class Draw {
 					for (double k:lengths)
 						if (k > max)
 							max = k;
-
+						if (numberOfPoints > 2)
 					lengths.remove(max);
 
 
 					if (Math.abs(lengths.get(0) - lengths.get(1)) < 1.0)
-						str.print("0 0 0");
+						str.print("0 0 0 ");
 					else
-						str.print("255 255 255");
+						str.print("255 255 255 ");
 
 			}
 		}
