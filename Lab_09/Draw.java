68c77e99ad307fb984b405601b1b822469bc9c2a
hubert-mazur <hiusvii@gmail.com>
2019-12-12 14:12:05 +0100

    update

--- Lab_09/Draw.java
+++ Lab_09/Draw.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Collections;
 
 public class Draw {
 	private int xSize;
@@ -13,14 +14,14 @@ public class Draw {
 	private PrintStream str;
 	private File fileOut;
 	private ArrayList<Double> lengths;
+
 	public Draw(String inputFile) {
 
 		file = new File(inputFile);
 
 		try {
 			reader = new Scanner(file);
-		}
-		catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			return;
 		}
 		this.xSize = reader.nextInt();
@@ -29,65 +30,64 @@ public class Draw {
 		this.output = new String(reader.next());
 		this.numberOfPoints = reader.nextInt();
 		points = new ArrayList<ArrayList<Integer>>(numberOfPoints);
-		
-		for (int i=0;i<numberOfPoints;i++)
+
+		for (int i = 0; i < numberOfPoints + 1; i++)
 			points.add(new ArrayList<Integer>());
 
-		for (int i=0;i<numberOfPoints;i++) {
+		for (int i = 0; i < numberOfPoints; i++) {
 			points.get(i).add(reader.nextInt());
 			points.get(i).add(reader.nextInt());
 
 		}
 
-
 		lengths = new ArrayList<Double>();
-		for  (int i =0;i<numberOfPoints;i++)
-		lengths.add(0.0);
+		for (int i = 0; i < numberOfPoints + 1; i++)
+			lengths.add(0.0);
+
+		for (int p = 0; p < numberOfPoints; p++) {
+			// lengths.set(p, Math.pow(i - points.get(p).get(0),2) + Math.pow(j -
+			// points.get(p).get(1), 2));
+			lengths.set(p, 0.0);
+		}
+
 	}
 
 	public void run() {
 		fileOut = new File(output);
 		try {
 			str = new PrintStream(fileOut);
-		} catch(FileNotFoundException e) {return;}
+		} catch (FileNotFoundException e) {
+			return;
+		}
 
 		str.print("P3\n");
 		str.print("#Hubert Mazur\n");
 		str.print(xSize + " " + ySize + "\n");
 		str.print("255\n");
 
+		for (int i = 0; i < xSize; i++) {
+			for (int j = 0; j < ySize; j++) {
+				for (int p = 0; p < numberOfPoints; p++) {
+					lengths.set(p, Math.sqrt(Math.pow(i - points.get(p).get(0), 2) + Math.pow(j - points.get(p).get(1), 2)));
 
-		for (int i=0;i<xSize;i++) {
-			for (int j=0;j<ySize;j++) {
-				for (int p=0;p<numberOfPoints;p++) {
-				lengths.set(p, Math.pow(i - points.get(p).get(0),2) + Math.pow(j - points.get(p).get(1), 2));
-				// lengths.set(p,0.0);
-
-				if (i == points.get(p).get(0) && j == points.get(p).get(1)) {
-					str.print("255 0 0 ");
-					continue;
-				}
+					if (i == points.get(p).get(0) && j == points.get(p).get(1)) {
+						str.print("255 0 0 ");
+						continue;
+					}
 				}
 
-					double max = lengths.get(0);
-					for (double k:lengths)
-						if (k > max)
-							max = k;
-						if (numberOfPoints > 2)
-					lengths.remove(max);
+				Collections.sort(lengths);
 
-
-					if (Math.abs(lengths.get(0) - lengths.get(1)) < 1.0)
-						str.print("0 0 0 ");
-					else
-						str.print("255 255 255 ");
+				if (Math.abs(lengths.get(0) - lengths.get(1)) < 1.0)
+					str.print("0 0 0 ");
+				else
+					str.print("255 255 255 ");
 
 			}
 		}
-		
-	str.close();
+
+		str.close();
 
 	}
-
 
 }
